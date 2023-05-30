2f4833d4f0c179856c21215a53c570c28ee0f758
Ian Edwin Kitembe <ianedwin@outlook.com>
2023-05-30 10:15:25 +0300

    log formatter

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -4,6 +4,28 @@ File: filtered_logger.py
 '''
 import re
 from typing import List
+import logging
+
+
+class RedactingFormatter(logging.Formatter):
+    '''
+    Class redacting formatter
+    '''
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super().__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        log_message = record.getMessage()
+        for field in self.fields:
+            log_message = filter_datum([field], self.REDACTION,
+                                       log_message, self.SEPARATOR)
+        record.msg = log_message
+        return super().format(record)
 
 
 def filter_datum(fields: List[str], redaction: str, message: str,
