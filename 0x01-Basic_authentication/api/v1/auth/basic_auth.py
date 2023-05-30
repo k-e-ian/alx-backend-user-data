2ce63c7e1406d075bb47c24df1c0ca345469c8e8
Ian Edwin Kitembe <ianedwin@outlook.com>
2023-05-30 17:50:46 +0300

    Basic Auth

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3.8
+'''
+File: basic_auth.py
+'''
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """
+    Class BasicAuth to manage API authentication
+    """
+    pass
