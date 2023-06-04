05f13da21df1a534ea48c06e2ac1c580a04246cc
Ian Edwin Kitembe <ianedwin@outlook.com>
2023-06-04 21:01:25 +0300

    empty session

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -2,9 +2,10 @@
 '''
 File: session_auth.py
 '''
+from api.v1.auth.auth import Auth
 
 
-class SessionAuth:
+class SessionAuth(Auth):
     """
     Class SessionAuth to manage API authentication
     """
