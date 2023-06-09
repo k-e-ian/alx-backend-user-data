a2949efad74fa8277fb5fb68c9854f2d2beb384c
Ian Edwin Kitembe <ianedwin@outlook.com>
2023-06-10 00:01:49 +0300

    app.py

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -11,7 +11,7 @@ import uuid
 def _hash_password(password: str) -> bytes:
     """method that takes in a password string args and returns bytes."""
     salt = bcrypt.gensalt()
-    hashed_password = bcrypt.hashpw(password.encode('utf-8'), s    alt)
+    hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
     return hashed_password
 
 
