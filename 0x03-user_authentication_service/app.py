073d082674af2e8542d9dffa9a20c1a0906a593b
Ian Edwin Kitembe <ianedwin@outlook.com>
2023-06-09 23:46:56 +0300

    login session

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -28,5 +28,19 @@ def register_user() -> str:
         return jsonify({"message": "email already registered"}), 400
 
 
+@app.route('/sessions', methods=['POST'])
+def login() -> str:
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    if AUTH.valid_login(email, password):
+        session_id = AUTH.create_session(email)
+        response = jsonify({'email': email, 'message': 'logged in'})
+        response.set_cookie('session_id', session_id)
+        return response
+    else:
+        abort(401)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
