125dba92d2f528a7e5d1e6726afbc9e5e7e30242
Ian Edwin Kitembe <ianedwin@outlook.com>
2023-06-09 23:23:20 +0300

    register user endpoint

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,14 +2,31 @@
 '''
 File: app.py
 '''
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
 
 app: Flask = Flask(__name__)
+AUTH = Auth()
+
 
 @app.route("/")
 def welcome() -> str:
+    """return a JSON payload of the form"""
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route("/users", methods=["POST"])
+def register_user() -> tuple:
+    """endpoint to register a user"""
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    try:
+        user = AUTH.register_user(email, password)
+        return jsonify({"email": user.email, "message": "user created"}), 200
+    except ValueError as e:
+        return jsonify({"message": str(e)}), 400
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
-
